4f55ee8f06f139cef0c67397166c8752544e6a5b
Mat Malone <mmalone@datacare.com>
2022-02-26 21:35:03 -0800

    more

--- matrix.py
+++ matrix.py
@@ -2,9 +2,12 @@ import numpy as np
 
 x1 = np.arange(16).reshape(4, 4)
 print(x1)
-x2 = np.transpose(np.arange(4).reshape(1, 4))
+x2 = np.arange(4).reshape(1, 4).transpose()
 print(x2)
 
 y = np.dot(x1, x2)
 print(y)
 
+
+t1 = np.arange(27).reshape(3, 3, 3)
+print(t1)
